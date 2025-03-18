3288e1ec446f26b0807bd8629e7cde8a4d02dcc5
Michael Warwick <michael.warwick@wam.studio>
2025-03-18 15:29:12 +1300

    New folder structure to allow for future expansion Removed nulls from Author and User schemas

--- src/lyricSanctuary.py
+++ src/lyricSanctuary.py
@@ -1,14 +1,15 @@
 # lyricSanctuary.py - Main application entry point
 
-from lyric_sanctuary.adapters.cli_ui import CliUI
-from lyric_sanctuary.adapters.lyrics_api import LyricsApiAdapter
-from lyric_sanctuary.application.lyrics_service import LyricsService
-from lyric_sanctuary.domain.lyrics_repository import LyricsRepository
+from lyric_app.adapters.cli_ui import CliUI
+from lyric_app.adapters.lyrics_api import LyricsApiAdapter
+from lyric_app.application.lyrics_service import LyricsService
+from lyric_app.domain.lyrics_repository import LyricsRepository
+
 
 def main():
     # Infrastructure layer (Adapters)
     lyrics_api = LyricsApiAdapter()
-    lyrics_repository = LyricsRepository(lyrics_api) #could also be a db
+    lyrics_repository = LyricsRepository(lyrics_api)  # could also be a db
 
     # Application layer (Service)
     lyrics_service = LyricsService(lyrics_repository)
@@ -16,6 +17,7 @@ def main():
     # Presentation layer (UI)
     cli_ui = CliUI(lyrics_service)
     cli_ui.run()
+
 
 if __name__ == "__main__":
     main()
